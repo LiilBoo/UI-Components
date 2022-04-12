65d790e2858e481b425e18ae9fd98dcc7f508a96
LiilBoo <liltrainer@pm.me>
2022-04-12 20:52:34 +0200

    Menu rotating

---
+++ Menu-rotating/Menu-circle.js
@@ -0,0 +1,9 @@
+const containerMenu = document.querySelector('.container-menu');
+
+const btnMenu = document.querySelector('.btn-menu');
+
+btnMenu.addEventListener('click', () => {
+
+    containerMenu.classList.toggle('active');
+
+});
